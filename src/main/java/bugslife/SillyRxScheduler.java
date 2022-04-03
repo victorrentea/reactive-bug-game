af4e3728750ba1bc84ce04c3d82b4f403a0c2d6b
Victor Rentea <victorrentea@gmail.com>
2022-04-04 00:55:00 +0300

    initial

---
+++ src/main/java/bugslife/SillyRxScheduler.java
@@ -0,0 +1,33 @@
+package bugslife;
+
+import javafx.application.Platform;
+import reactor.core.Disposable;
+import reactor.core.scheduler.Scheduler;
+import reactor.util.annotation.NonNull;
+
+public class SillyRxScheduler implements Scheduler {
+   @Override
+   @NonNull
+   public Disposable schedule(@NonNull Runnable task) {
+      Platform.runLater(task);
+      return () -> { };
+   }
+
+   @Override
+   @NonNull
+   public Worker createWorker() {
+      return new Worker() {
+         @Override
+         @NonNull
+         public Disposable schedule(@NonNull Runnable task) {
+            Platform.runLater(task);
+            return () -> { };
+         }
+
+         @Override
+         public void dispose() {
+
+         }
+      };
+   }
+}
